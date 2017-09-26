f2c5cba36ba904fefcec9d9fa90c3a283d23893b
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2017-09-26 13:11:50 +0200

    update

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-**Clustering Illumina MiSeq paired-end reads of MHC sequences**
-
-Clustering of paired-end reads is conducted using the script `mhc_clustering.py`.
-
-```
-mhc_clustering.py -h
-```
-
-The subfolder `lib` contains required functions and may contain hidden marko models created from multiple sequence alignment of published mhc alleles using **HMMER3**.  
+**Clustering mhc class II sequences**
 
 *This repository was forked from https://github.com/nextgenusfs/mhc_cluster.git` and subsequently modified. See details there.*
 
-**Dependent software and installation**
+The script `cluster_mhc.py` allows to cluster mhc class II sequences into `'OTUs'` that represent putatitive alleles. Input sequences are `FASTQ` files generated on a Illumina MiSeq run. The following steps may be executed outside the clustering:
+
+* Extracting barcodes from sequences (`QIIME extract_barcodes.py`)
+* Merging paired-end reads (e.g. `vsearch ---fastq_mergepairs`)
+* Stripping primer sequences and extracting full length exon sequences (e.g. `fastx_trimmer`)
+* Adding barcodes to sequence headers (e.g. `custom R scripts`)
+
+### Using `cluster_mhc.py`
+
+```
+## helpfile
+cluster_mhc.py -h
+## usage
+cluster_mhc.py -f reads.fastq -o output_name
+```
+
+The subfolder `lib` contains required functions and may contain hidden marko models created from multiple sequence alignment of published mhc alleles using `HMMER3` for both nucleotide and amino acid sequences.  
+
+### Dependent software
+
 The installation of some of these may cause some trouble and requires different attempts depending on system and the rights of the user (*root* etc...)
 
 * USEARCH8 (http://www.drive5.com/usearch)
--- cluster_mhc.py
+++ cluster_mhc.py
@@ -4,17 +4,13 @@
 # ===================================================================================
 #
 # This scripts does the following main steps:
-# 1.) Filterering reads based on expted error rates using vsearch
-# 2.) Removes reads reflecting contamination by mapping reads to a mhc reference (hmm model)
-# 3.) Trims/pads ends of all reads to average length (using defaults)
-# 4.) De-replication of reads
-# 5.) Sorts reads by frequency is descending order
-# 6.) Clusters reads into OTUs based on identity threshold
-# 7.) Maps reads back to OTUs
-# 8.) Creates OTU table
-# 9.) Translates OTUs to amino acid sequences 
-# 10.) Filters AAS against hidden markov model 
-#
+# 1.) Filter reads against an hidden markov model comprising mhc a multiple sequence alignment
+#	of known mhc sequcens from various mammals. (Computationally demanding!)
+# 2.) Quality filter reads prior to cluster generation based on expected error rates
+# 3.) Find unique sequences and estimate their abundance for the filtered reads
+# 4.) Cluster filtered reads into OTUs 
+# 5.) Map all reads to the OTUs
+
 # Perequisites are:
 # 	A hidden markov model may be constructed based on a multiple sequence alignment and stored in the 
 # 	subfolder 'lib'.
@@ -26,7 +22,7 @@
 #	Create help files
 #	hmmpress <hmmbuild output>
 #
-# Modified based on the script 'mhc-OTU_cluster.py':
+# Principal outline adapted from the script 'mhc-OTU_cluster.py':
 # Palmer et al (2016). PeerJ PrePrints 4 (2016): e1662v1
 # https://github.com/nextgenusfs/mhc_cluster
 # 
@@ -51,17 +47,18 @@ parser=argparse.ArgumentParser(prog='mhc_clustering.py', usage="%(prog)s [option
     epilog="""Meinolf Ottensmann (2017) https://github.com/mottensmann/mhc_cluster""",
     formatter_class=MyFormatter)
 
-parser.add_argument('-f','--fastq', dest="FASTQ", required=True, help='FASTQ file')
-parser.add_argument('-o','--out', default='out', help='Base output name')
-parser.add_argument('-e','--maxee', default='1.0', help='Quality trim EE value')
+parser.add_argument('-f','--fastq', dest="FASTQ", required=True, help='FASTQ input file')
+parser.add_argument('-o','--out', default='out', help='Path and name of the output')
+parser.add_argument('-e','--maxee', default='1.0', help='Expected error rate for filtering')
 parser.add_argument('-l','--length', default='auto', help='Trim Length')
-parser.add_argument('-p','--pct_otu', default=99, help="OTU Clustering Percent")
+parser.add_argument('-p1','--pct_otu', default='1', help="100 - Identity threshold for clustering [%]")
+parser.add_argument('-p2','--pct_mapping', default='0.97', help="Identity threshold for mapping reads to OTUs")
 parser.add_argument('-n','--num_diff', default='False', help="OTU Clustering Number of differences")
 parser.add_argument('-m','--minsize', default='2', help='Min abundance to keep for clustering')
 parser.add_argument('-u','--usearch', dest="usearch", default='usearch8', help='USEARCH8 EXE')
-parser.add_argument('-hmm','--hmm', dest="hmm", default='urth_dqb.hmm', help='Hidden markov model reference')
+parser.add_argument('-hmm','--hmm', dest="hmm", default='mammalia_dqb.hmm', help='Hidden markov model reference')
 parser.add_argument('-hmm_aas','--hmm_aas', dest="hmm_aas", default='mhcII_beta_aas.hmm', help='Hidden markov model reference of amino acid sequences')
-parser.add_argument('-v','--vsearch', dest="vsearch", default='vsearch', help='VSEARCH')
+parser.add_argument('--run_hmm', action='store_true', help='Filter raw reads against Hidden markov model')
 parser.add_argument('-c','--cpus', default=4, help='Number of cpus')
 parser.add_argument('--translate', action='store_true', help='Translate OTUs')
 args=parser.parse_args()
@@ -94,7 +91,7 @@ if os.path.isfile(log_name):
 log_file = open(log_name, 'ab')
 
 usearch = args.usearch
-vsearch = args.vsearch
+
 try:
     subprocess.call([usearch, '--version'], stdout = log_file, stderr = log_file)
 except OSError:
@@ -104,8 +101,9 @@ except OSError:
 if args.num_diff != 'False':
     print "\nWarning:  --num_diff %s was specified, this will override the --pct_otu option" % args.num_diff
 
+	
 # set up log file for prints to the console
-console_log = args.out + '_console_log.txt'
+console_log = args.out + '.EE' + args.maxee + '.console_log.txt'
 
 file = open(console_log, "w")
 file.write('\nLoading records: ' + '{0:,}'.format(countfastq(args.FASTQ)) + ' reads\n') 
@@ -113,36 +111,116 @@ file.close()
 
 # Count input reads
 print '\nLoading records: ' + '{0:,}'.format(countfastq(args.FASTQ)) + ' reads'
+
+# Convert to fasta to use HMMER3
+
+raw_reads_fasta = args.out + '.EE' + args.maxee + '.raw_reads.fa'
+subprocess.call([usearch, '-fastq_filter', args.FASTQ, '-fastq_qmax', '45', '-fastaout', raw_reads_fasta], stdout = log_file, stderr = log_file)
+
+# 1. Run HMMER3 to filter contaminant sequences out.
+# ##################################################
+FNULL = open(os.devnull, 'w')
+all_reads_hmm_out = args.out + '.EE' + args.maxee + '.raw_reads.hmm.txt'
+if args.run_hmm:
+	hmm = script_path + '/lib/' + args.hmm 
+	print "CMD: Filter all reads against HMM \nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, all_reads_hmm_out, hmm, raw_reads_fasta)
+	file = open(console_log, "a")
+	file.write("\nCMD: Filter all reads against HMM \nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, all_reads_hmm_out, hmm, raw_reads_fasta)) 
+	file.close()
+ 
+	subprocess.call(['hmmscan', '--cpu', cpus, '--domtblout', all_reads_hmm_out, hmm, raw_reads_fasta], stdout = FNULL, stderr = FNULL)
+
+# Parse HMMer results
+	file = open(console_log, "a")
+	file.write("\nCMD: Filtering HMM results\n") 
+	file.close()
+
+	print "CMD: Filtering HMM results"
+	q_list = []
+	l_list = []
+	counter = 0
+	hmmer_raw_results = open(all_reads_hmm_out, 'r')
+	for qresult in SearchIO.parse(hmmer_raw_results, "hmmscan3-domtab"):
+		q_list.append(qresult.id)
+		l_list.append(qresult.seq_len)
+		counter += 1
+	hmmer_raw_results.close()
+
+	sum_1 = sum(l_list)
+	avg_len = sum_1 / counter
+
+	if args.length == 'auto':
+		trim_len = avg_len
+	else:
+		trim_len = int(args.length)
+
+	file = open(console_log, "a")
+	file.write("%10u sequences passed filter" % counter) 
+	file.write("%10u bp is average length" % avg_len) 
+	file.close()
+
+	print "%10u sequences passed filter" % counter
+	print "%10u bp is average length" % avg_len
+
+	file = open(console_log, "a")
+	file.write("\nCMD: Retrieving results and trimming/padding to %i bp for clustering" % trim_len) 
+	file.close()
+    
+	print "\nCMD: Retrieving results and trimming/padding to %i bp for clustering" % trim_len
+# Retrieve sequences in the "pass" list by looping through the query list, make same length
+	raw_pass_out = args.out + '.EE' + args.maxee + '.raw_reads.hmm.pass.fa'
+	pass_handle = open(raw_pass_out, 'wb')
+	filtered = SeqIO.parse(raw_reads_fasta, "fasta")
+	for rec in filtered:
+		if rec.id in q_list:
+			L = len(rec.seq)
+			if L < trim_len:
+				Seq = rec.seq + (trim_len - L)*'N'
+			else:
+				T = trim_len - 1
+				Seq = rec.seq[:T]
+			pass_handle.write(">%s\n%s\n" % (rec.id, Seq))
+	pass_handle.close()
+
+	file = open(console_log, "a")
+	file.write("\nOutput: " + '{0:,}'.format(countfasta(raw_pass_out)) + ' reads\n') 
+	file.close()
+
+	print "Output: " + '{0:,}'.format(countfasta(raw_pass_out)) + ' reads\n'
+
+# 2.) Quality filter reads based on expected errors before clustering OTUs
+# ########################################################################
     
 # Run vsearch fastq filtering step, output to fasta
 filter_out = args.out + '.EE' + args.maxee + '.filter.fa'
-print "\nCMD: Quality Filtering\n%s -fastq_filter %s -fastq_maxee %s -fastq_qmax 45 -fastaout %s" % (vsearch, args.FASTQ, args.maxee, filter_out)
+print "\nCMD: Quality Filtering\n%s -fastq_filter %s -fastq_maxee %s -fastq_qmax 45 -fastaout %s" % (usearch, args.FASTQ, args.maxee, args.FASTQ)
 
 file = open(console_log, "a")
-file.write("\nCMD: Quality Filtering\n%s -fastq_filter %s -fastq_maxee %s -fastq_qmax 45 -fastaout %s" % (vsearch, args.FASTQ, args.maxee, filter_out)) 
+file.write("\nCMD: Quality Filtering\n%s -fastq_filter %s -fastq_maxee %s -fastq_qmax 45 -fastaout %s" % (usearch, args.FASTQ, args.maxee, filter_out)) 
 file.close() 
 
-subprocess.call([vsearch, '-fastq_filter', args.FASTQ, '-fastq_maxee', args.maxee, '-fastq_qmax', '45', '-fastaout', filter_out], stdout = log_file, stderr = log_file)
+subprocess.call([usearch, '-fastq_filter', args.FASTQ, '-fastq_maxee', args.maxee, '-fastq_qmax', '45', '-fastaout', filter_out], stdout = log_file, stderr = log_file)
+
 print "Output: " + '{0:,}'.format(countfasta(filter_out)) + ' reads\n'
 
 file = open(console_log, "a")
 file.write("\nOutput: " + '{0:,}'.format(countfasta(filter_out)) + ' reads\n') 
 file.close() 
 
-# Run HMMer3 to filter contaminant sequences out.
-hmm_out = args.out + '.EE' + args.maxee + '.mhc.hmm.txt'
+# Now, filter against hmm again
+
+hmm_filtered_out = args.out + '.EE' + args.maxee + '.reads.filtered.hmm.txt'
 hmm = script_path + '/lib/' + args.hmm 
-FNULL = open(os.devnull, 'w')
-print "CMD: Running HMMER3 using MHC HMM model (using %s cpus)\nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, cpus, hmm_out, hmm, filter_out)
+
+print "CMD: Filter filtered reads against HMM \nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, hmm_filtered_out, hmm, filter_out)
 
 file = open(console_log, "a")
-file.write("\nCMD: Running HMMER3 using MHC HMM model (using %s cpus)\nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, cpus, hmm_out, hmm, filter_out)) 
+file.write("\nCMD: Filter filtered reads against HMM \nhmmscan --cpu %s --domtblout %s %s %s\n" % (cpus, hmm_filtered_out, hmm, filter_out)) 
 file.close()
 
-subprocess.call(['hmmscan', '--cpu', cpus, '--domtblout', hmm_out, hmm, filter_out], stdout = FNULL, stderr = FNULL)
+subprocess.call(['hmmscan', '--cpu', cpus, '--domtblout', hmm_filtered_out, hmm, filter_out], stdout = FNULL, stderr = FNULL)
 
 # Parse HMMer results
-
 file = open(console_log, "a")
 file.write("\nCMD: Filtering HMM results\n") 
 file.close()
@@ -151,15 +229,19 @@ print "CMD: Filtering HMM results"
 q_list = []
 l_list = []
 counter = 0
-hmmer_results = open(hmm_out, 'r')
+hmmer_results = open(hmm_filtered_out, 'r')
 for qresult in SearchIO.parse(hmmer_results, "hmmscan3-domtab"):
     q_list.append(qresult.id)
     l_list.append(qresult.seq_len)
     counter += 1
 hmmer_results.close()
+sum_2 = sum(l_list)
+avg_len = sum_2 / counter
 
-sum = sum(l_list)
-avg_len = sum / counter
+if args.length == 'auto':
+    trim_len = avg_len
+else:
+    trim_len = int(args.length)
 
 file = open(console_log, "a")
 file.write("%10u sequences passed filter" % counter) 
@@ -169,19 +251,14 @@ file.close()
 print "%10u sequences passed filter" % counter
 print "%10u bp is average length" % avg_len
 
-if args.length == 'auto':
-    trim_len = avg_len
-else:
-    trim_len = int(args.length)
-
 file = open(console_log, "a")
 file.write("\nCMD: Retrieving results and trimming/padding to %i bp for clustering" % trim_len) 
 file.close()
     
 print "\nCMD: Retrieving results and trimming/padding to %i bp for clustering" % trim_len
 # Retrieve sequences in the "pass" list by looping through the query list, make same length
-pass_out = args.out + '.EE' + args.maxee + '.hmm.pass.fa'
-pass_handle = open(pass_out, 'wb')
+filtered_pass_out = args.out + '.EE' + args.maxee + '.hmm.filtered.pass.fa'
+pass_handle = open(filtered_pass_out, 'wb')
 filtered = SeqIO.parse(filter_out, "fasta")
 for rec in filtered:
     if rec.id in q_list:
@@ -195,20 +272,25 @@ for rec in filtered:
 pass_handle.close()
 
 file = open(console_log, "a")
-file.write("\nOutput: " + '{0:,}'.format(countfasta(pass_out)) + ' reads\n') 
+file.write("\nOutput: " + '{0:,}'.format(countfasta(filtered_pass_out)) + ' reads\n') 
 file.close()
 
-print "Output: " + '{0:,}'.format(countfasta(pass_out)) + ' reads\n'
+print "Output: " + '{0:,}'.format(countfasta(filtered_pass_out)) + ' reads\n'
 
-# Run Usearch8 full length dereplication
+# 3.) De-replication of reads
+# ###########################
 derep_out = args.out + '.EE' + args.maxee + '.derep.fa'
+sort_out = args.out + '.EE' + args.maxee + '.sort.fa'
+
 
 file = open(console_log, "a")
-file.write("\nCMD: De-replication\n%s -derep_fulllength %s -sizeout -fastaout %s" % (usearch, pass_out, derep_out)) 
+file.write("\nCMD: De-replication\n%s -derep_fulllength %s -sizeout -fastaout %s" % (usearch, filter_out, derep_out)) 
 file.close()
 
-print "CMD: De-replication\n%s -derep_fulllength %s -sizeout -fastaout %s" % (usearch, pass_out, derep_out)
-subprocess.call([usearch, '-derep_fulllength', pass_out, '-sizeout', '-fastaout', derep_out], stdout = log_file, stderr = log_file)
+print "CMD: De-replication\n%s -derep_fulllength %s -sizeout -fastaout %s" % (usearch, filter_out, derep_out)
+
+subprocess.call([usearch, '-derep_fulllength', filter_out, '-sizeout', '-fastaout', derep_out], stdout = log_file, stderr = log_file)
+subprocess.call([usearch, '-sortbysize', derep_out, '-minsize', args.minsize, '-fastaout', sort_out], stdout = log_file, stderr = log_file)
 
 print "Output: " + '{0:,}'.format(countfasta(derep_out)) + ' reads\n'
 
@@ -216,34 +298,16 @@ file = open(console_log, "a")
 file.write("\nOutput: " + '{0:,}'.format(countfasta(derep_out)) + ' reads\n') 
 file.close()
 
+# 4.) Cluster into OTUs
+# #####################
 
-# Run usearch 8 to sort by size
-sort_out = args.out + '.EE' + args.maxee + '.sort.fa'
-
-file = open(console_log, "a")
-file.write("\nCMD: Sorting by Size\n%s -sortbysize %s -minsize %s -fastaout %s\n" % (usearch, derep_out, args.minsize, sort_out)) 
-file.close()
-
-print "CMD: Sorting by Size\n%s -sortbysize %s -minsize %s -fastaout %s\n" % (usearch, derep_out, args.minsize, sort_out)
-subprocess.call([usearch, '-sortbysize', derep_out, '-minsize', args.minsize, '-fastaout', sort_out], stdout = log_file, stderr = log_file)
-
-# Run clustering algorithm
-if args.num_diff == 'False':
-    radius = str(100 - float(args.pct_otu))
-    mapping_pct = str(float(args.pct_otu) / 100)
-else:
-    diff = int(args.length) - int(args.num_diff)
-    percent = float(diff) / float(args.length)
-    radius = str(100 - (100 * percent))
-    mapping_pct = "{:0.3f}".format(percent)
 otu_out = args.out + '.EE' + args.maxee + '.otus.fa'
-
 file = open(console_log, "a")
-file.write("\nCMD: Clustering OTUs\n%s -cluster_otus %s -sizein -sizeout -relabel MHC_ -otu_radius_pct %s -otus %s" % (usearch, sort_out, radius, otu_out)) 
+file.write("\nCMD: Clustering OTUs\n%s -cluster_otus %s -sizein -sizeout -relabel MHC_ -otus %s" % (usearch, sort_out, otu_out)) 
 file.close()
 
-print "CMD: Clustering OTUs\n%s -cluster_otus %s -sizein -sizeout -relabel MHC_ -otu_radius_pct %s -otus %s" % (usearch, sort_out, radius, otu_out)
-subprocess.call([usearch, '-cluster_otus', sort_out, '-sizein', '-relabel', 'MHC_', '-otu_radius_pct', radius, '-otus', otu_out], stdout = log_file, stderr = log_file)
+print "CMD: Clustering OTUs\n%s -cluster_otus %s -sizein -sizeout -relabel MHC_ -otu_radius_pct %s -otus %s" % (usearch, sort_out, args.pct_otu, otu_out)
+subprocess.call([usearch, '-cluster_otus', sort_out, '-sizein', '-relabel', 'MHC_', '-otu_radius_pct', args.pct_otu, '-otus', otu_out], stdout = log_file, stderr = log_file)
 
 # Fix OTUs, remove trailing N's
 fix_otus = args.out + '.EE' + args.maxee + '.fixed.otus.fa'
@@ -262,16 +326,22 @@ file.write("%10u total OTUs\n" % otu_count)
 file.close()
 
 print "%10u total OTUs\n" % otu_count
-    
-# Map reads back to OTUs
+
+# 5.) Map reads back to OTUs
+# ###########################
+
+if args.run_hmm == False:
+	raw_pass_out = raw_reads_fasta
+
 uc_out = args.out + '.EE' + args.maxee + '.mapping.uc'
+otu_table = args.out + '.EE' + args.maxee + '.otu_table.txt'
 
 file = open(console_log, "a")
-file.write("\nCMD: Mapping Reads to OTUs\n%s -usearch_global %s -strand plus -id %s -db %s -uc %s\n" % (usearch, pass_out, mapping_pct, fix_otus, uc_out)) 
+file.write("\nCMD: Mapping Reads to OTUs\n%s -otutab %s -strand plus -otutabout %s -mapout %s\n" % (usearch, raw_pass_out, otu_table, uc_out)) 
 file.close()
 
-print "CMD: Mapping Reads to OTUs\n%s -usearch_global %s -strand plus -id %s -db %s -uc %s\n" % (usearch, pass_out, mapping_pct, fix_otus, uc_out)
-subprocess.call([usearch, '-usearch_global', pass_out, '-strand', 'plus', '-id', mapping_pct, '-db', fix_otus, '-uc', uc_out], stdout = log_file, stderr = log_file)
+print "CMD: Mapping Reads to OTUs\n%s -usearch_global %s -strand plus -id %s -db %s -uc %s\n" % (usearch, raw_pass_out, args.pct_mapping, fix_otus, uc_out)
+subprocess.call([usearch, '-usearch_global', raw_pass_out, '-strand', 'plus', '-id', args.pct_mapping, '-db', fix_otus, '-uc', uc_out], stdout = log_file, stderr = log_file)
 
 # Build OTU table
 otu_table = args.out + '.EE' + args.maxee + '.otu_table.txt'
@@ -373,7 +443,7 @@ with open(filter_out, 'rU') as input:
             else:
                 BarcodeCountB[ID] += 1
 BarcodeCountC = {}
-with open(pass_out, 'rU') as input:
+with open(raw_pass_out, 'rU') as input:
     for line in input:
         if line.startswith('>'):
             ID = line.split("=")[-1].split(";")[0]
@@ -381,7 +451,7 @@ with open(pass_out, 'rU') as input:
                 BarcodeCountC[ID] = 1
             else:
                 BarcodeCountC[ID] += 1
-bc_count = args.out + args.maxee + '.barcode.counts.txt'
+bc_count = args.out + '.EE' + args.maxee + '.barcode.counts.txt'
 with open(bc_count, 'w') as output:
     output.write("Barcode\tDemux_total\tFilter_total\tHMM_total\n")               
     for k,v in natsorted(BarcodeCountA.items(), key=lambda (k,v): v, reverse=True):
@@ -395,19 +465,21 @@ with open(bc_count, 'w') as output:
 print "\n------------------------------------------------"
 print "OTU Clustering Script has Finished Successfully"
 print "------------------------------------------------"
-print ("Input FASTQ:           %s" % (args.FASTQ))
-print ("Filtered FASTQ:        %s" % (filter_out))
-print ("HMM Pass FASTA:        %s" % (pass_out))
-print ("Dereplicated FASTA:    %s" % (derep_out))
-print ("Sorted FASTA:          %s" % (sort_out))
-print ("Clustered OTUs:        %s" % (fix_otus))
+print ("Input fastq format:	%s" % (args.FASTQ))
+print ("Input fasta format:	%s" % (raw_reads_fasta))
+print ("HMM passed raw reads:	%s" % (raw_pass_out))
+print ("Filtered reads:	%s" % (filter_out))
+print ("HMM passed filtered reads:	%s" % (filtered_pass_out))
+print ("Dereplicated reads:	%s" % (derep_out))
+print ("Reads sorted by frequency:	%s" % (derep_out))
+print ("Clustered OTUs:	%s" % (fix_otus))
 if args.translate:
-    print ("Translated OTUs:       %s" % (pass_prot))
-    print ("Translated Unique:     %s" % (concat_out))
-print ("usearch Mapping file:  %s" % (uc_out))
-print ("OTU Table:             %s" % (otu_table))
-print ("usearch LogFile:       %s" % (log_name))
-print ("Reads per Barcode:     %s" % (bc_count))
-print "------------------------------------------------"
+    print ("Translated OTUs:	%s" % (pass_prot))
+    print ("Translated Unique:	%s" % (concat_out))
+print ("usearch Mapping file:	%s" % (uc_out))
+print ("OTU Table:	%s" % (otu_table))
+print ("LogFile:	%s" % (console_log))
+print ("Reads per Barcode:	%s" % (bc_count))
+print "---------------------------------------------------------------"
 
  
